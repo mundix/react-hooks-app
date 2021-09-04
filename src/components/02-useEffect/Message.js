a9258c996a434283ffd9c23031044ba3c043912b
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-09-04 09:32:40 -0400

    Montando y desmontando event listener probando con coordenadas del mouse

--- src/components/02-useEffect/Message.js
+++ src/components/02-useEffect/Message.js
@@ -1,11 +1,23 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 export const Message = () => {
+
+    const [coords, setCoords] = useState({x:0, y:0});
+    const {x, y} = coords;
 
     useEffect(() => {
         console.log('Component Montado');
         // ESte retorno de useEffect puede consumir la memoria , ojo con eso. 
+        const mouseMove = (e) => {
+            const coords = {x: e.x, y: e.y };
+            setCoords(coords);
+            // console.log(':D');
+        }
+        window.addEventListener('mousemove', mouseMove);
+
         return () => {
+            // Remover el listener 
+            window.removeEventListener('mousemove', mouseMove);
             console.log('componetn desmontado');
         }
 
@@ -14,6 +26,9 @@ export const Message = () => {
     return (
         <div>
             <h3>Eres FGenial!</h3>
+            <p>
+                X: {x}, Y: {y}
+            </p>
         </div>
     )
 }
