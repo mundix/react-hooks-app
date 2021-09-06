9157bcb01d06c859b3ef0ef7c19eb53638930912
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-09-05 21:20:50 -0400

    Agregando formulario y estilo

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -20,14 +20,47 @@ export const TodoApp = () => {
     console.log(todos);
 
     return (
-        <div>
-            <h1>TodoApp</h1>
+        <>
+            <h1>TodoApp ({todos.length})</h1>
             <hr />
-            <ul>
-                <li>Hola</li>
-                <li>Mundo</li>
-                <li>Hola que tal</li>
+            <div className="row">
+                <div className="col-7">
+                <ul className='list-group list-group-flush'>
+                {
+                    todos.map( todo => (
+                        <li
+                            key={todo.id}
+                            className='list-group-item'
+                        >
+                            <p className="text-center">{ todo.desc }</p>
+                            <button className="btn btn-danger">
+                                Borrad
+                            </button>
+                        </li>
+                    ))
+                }
             </ul>
-        </div>
+                </div>
+                <div className="col-5">
+                    <h4>Agregar Todo</h4>
+                    <hr />
+                    <form action="">
+                        <input 
+                            type="text" 
+                            name='description'
+                            className='form-control'
+                            placeholder='Aprender ...'
+                            autoComplete='off'
+                        />
+                        <button
+                            className='btn btn-outline-primary mt-1 btn-block'
+                        >
+                            Agregar
+                        </button>
+                    </form>
+                </div>
+            </div>
+            
+        </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import ReactDOM from 'react-dom';
 // import { CallbackHook } from './components/06-memos/CallbackHook';
 // import { Padre } from './components/07-tarea-memo/Padre';
 import { TodoApp } from './components/08-useReducer/TodoApp';
-
+import './global.css';
 
 
 ReactDOM.render(
