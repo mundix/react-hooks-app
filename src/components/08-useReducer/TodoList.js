610449ce2aa7f2752e0d982aa81b86d063b1a8fd
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-09-05 23:28:13 -0400

    Se agrego el TodoListItem y compeltado la tarea

--- src/components/08-useReducer/TodoList.js
+++ src/components/08-useReducer/TodoList.js
@@ -1,31 +1,22 @@
 import React from 'react';
+import { TodoListItem } from './TodoListItem';
 
-export const TodoList = ({todos, handleDelete, handleToggle}) => {
+export const TodoList = ({ todos, handleDelete, handleToggle }) => {
     return (
         <>
             <ul className='list-group list-group-flush'>
-                        {
-                            todos.map((todo, i) => (
-                                <li
-                                    key={todo.id}
-                                    className='list-group-item'
-                                >
-                                    <p
-                                        className={`${todo.done && 'complete'}`}
-                                        onClick={() => handleToggle(todo.id)}
-
-                                    > {i + 1} - {todo.desc}</p>
-                                    <button
-                                        className="btn btn-danger"
-                                        // Como necesito pasar argumento , se envia un callback y la funciona con el argumento
-                                        onClick={() => handleDelete(todo.id)}
-                                    >
-                                        Borrad
-                                    </button>
-                                </li>
-                            ))
-                        }
-                    </ul>
+                {
+                    todos.map((todo, i) => (
+                        <TodoListItem
+                            key={todo.id}
+                            index={i}
+                            todo={todo}
+                            handleDelete={handleDelete}
+                            handleToggle={handleToggle}
+                        />
+                    ))
+                }
+            </ul>
         </>
     )
 }
