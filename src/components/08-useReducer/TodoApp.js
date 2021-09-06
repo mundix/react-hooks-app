86239a34a6d0e312a69f948bcce7cb066d9451c6
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-09-05 20:52:59 -0400

    Se creo el TodoReducer

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,12 +1,33 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { TodoReducer } from './TodoReducer';
 import './styles.css';
 
+const initialState = [{
+    id: new Date().getTime(),
+    desc: 'Aprender React',
+    done: false
+}];
+
+//El reducer puede crease en  un archivo independiente 'TodoReducer.js'
+
 export const TodoApp = () => {
+
+    //reducer => es la function reducer, 
+    //initialState es el valor inicial
+    //el init es una function par ainiciarlizar el state en caso de que el state sea utuilizado , se puede memorizar 
+    const [ todos ] = useReducer(TodoReducer, initialState);
+
+    console.log(todos);
+
     return (
         <div>
             <h1>TodoApp</h1>
             <hr />
-
+            <ul>
+                <li>Hola</li>
+                <li>Mundo</li>
+                <li>Hola que tal</li>
+            </ul>
         </div>
     )
 }
---
+++ src/components/08-useReducer/TodoReducer.js
@@ -0,0 +1,10 @@
+export const TodoReducer = (state = [], action) => {
+    switch (action.type) {
+        // case 'add':
+            
+            // break;
+    
+        default:
+            return state;
+    }
+}
