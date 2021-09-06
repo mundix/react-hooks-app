7e764f8b12558af96530ebbba146e0611ad4d0e2
Edmundo Pichardo <ce.pichardo@gmail.com>
2021-09-06 05:40:07 -0400

    Trabajando con Link y NavLink

--- src/components/09-useContext/AppRouter.js
+++ src/components/09-useContext/AppRouter.js
@@ -5,22 +5,35 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
+    Redirect
 } from "react-router-dom";
 
 import { AboutScreen } from './AboutScreen';
 import { LoginScreen } from './LoginScreen';
 import { HomeScreen } from './HomeScreen';
+import { NavBar } from './NavBar';
 
 export const AppRouter = () => {
     return (
         <Router>
             {/* Este es el fragment  */}
             <div>
+
+                <NavBar />
+
                 <Switch>
                     <Route exact path='/' component={HomeScreen} />
+
                     <Route exact path='/about' component={AboutScreen} />
                     <Route exact path='/login' component={LoginScreen} />
+
+                    {/* Estop es un default por si no encuentran nada, esto puede ser un 404 o se puede usar un redirect */}
+                    {/* <Router component={ HomeScreen }/> */}
+                    {/* Si no encuentra ninguna de estas direccion redirecciona al home  */}
+                    <Redirect to='/'/>
+
                 </Switch>
+
             </div>
         </Router>
     )
